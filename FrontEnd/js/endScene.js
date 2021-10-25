0af66ef53bc83c0a5422d4df8c3934c1d7b9f9c2
Ulrike Kuhl <ukuhl@techfak.uni-bielefeld.de>
2021-10-25 16:24:09 +0200

    add button to debriefing site

--- FrontEnd/js/endScene.js
+++ FrontEnd/js/endScene.js
@@ -13,6 +13,7 @@ class EndScene extends Phaser.Scene {
 		this.load.spritesheet('checkbox', 'static/CheckBoxSprites.png', { frameWidth: 51, frameHeight: 50 });
 		this.load.image('UBIE', 'static/UBF-logo.png');
 		this.load.image('ITSML', 'static/ITS_ML_logo.png');
+		this.load.image('buttonAgree', 'static/buttonSubmit.png');
 
 	}
 
@@ -32,13 +33,22 @@ class EndScene extends Phaser.Scene {
 			'',
 			'Note down this code, return to AMT and paste the code into the box to receive your payment.',
 			'',
-			'If you would like to know more about the aim of this work, visit: https://its-ml.de/index.php/pazdebrief/',
+			'If you would like to know more about the aim of this work, click the button below.',
 			'',
 			'In case you have further questions or comments regarding this study, please contact Dr. Ulrike Kuhl at Bielefeld University: ukuhl@techfak.uni-bielefeld.de .',
 			'',
 			'You may close this window now.',
 			''
 		]
+
+		// add button to start game and switch to fullscreen
+		var buttonDebrief = this.add.image(0, 0, 'buttonAgree').setScale(0.65)
+			.setInteractive()
+			.on('pointerdown', () => { window.open('https://its-ml.de/index.php/pazdebrief/'); });
+
+		//var textAgree = this.add.text(-55, -20, 'I agree to participate in this study.', { fontFamily: 'Arial', fontSize: '14px', color: '#000000' }).setOrigin(0);
+		var textDebrief = this.add.text(-85, -25, ['Tell me more','about the study!'], { fontSize: '18px', color: '#000000' }).setOrigin(0);
+		var buttonContainer = this.add.container(window.innerWidth * 0.7, window.innerHeight * 0.6, [buttonDebrief, textDebrief])
 
 		this.add.text(window.innerWidth * 0.05, window.innerHeight * 0.20, qIntro, { fontFamily: 'Arial', fontSize: '16px', color: '#000000' });
 
