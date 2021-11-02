8d62c94a1440df9b0591ddbeeab1d6d5a4357ff6
André Artelt <aartelt@techfak.uni-bielefeld.de>
2021-11-02 09:08:44 +0100

    BackEnd: Make sure database is always connected

--- BackEnd/dbmgr.py
+++ BackEnd/dbmgr.py
@@ -12,9 +12,12 @@ public_key_file = "public_key.bin"
 
 class DataMgr():
     def __init__(self):
-        self.db = mysql.connector.connect(host="localhost", user=user_name, password=user_pw, database=database)
+        self.db = self.__connect_to_database()
         self.public_key = load_key(public_key_file)
         self.__init_database()
+
+    def __connect_to_database(self):
+        return mysql.connector.connect(host="localhost", user=user_name, password=user_pw, database=database)
 
     def __init_database(self):
         self.db.cursor().execute("CREATE TABLE IF NOT EXISTS users (userId TEXT NOT NULL, controlGroup INT NOT NULL)")
@@ -27,8 +30,9 @@ class DataMgr():
 
     def add_new_user(self, user_id, control_group):
         try:
-            self.db.cursor().execute("INSERT INTO users (userId, controlGroup) VALUES(%s,%s)", (user_id, int(control_group)))
-            self.db.commit()
+            db = self.__connect_to_database()
+            db.cursor().execute("INSERT INTO users (userId, controlGroup) VALUES(%s,%s)", (user_id, int(control_group)))
+            db.commit()
 
             return True
         except Exception as ex:
@@ -37,7 +41,9 @@ class DataMgr():
 
     def get_user_by_userId(self, user_id):
         try:
-            cur = self.db.cursor()
+            db = self.__connect_to_database()
+
+            cur = db.cursor()
             cur.execute("SELECT controlGroup FROM users WHERE userId=%s", (user_id,))
             result = cur.fetchone()
 
@@ -48,15 +54,17 @@ class DataMgr():
 
     def log_questionnaire_answers(self, user_id, question_id, checkbox_values):
         try:
+            db = self.__connect_to_database()
+
             if question_id == -1:
-                self.db.cursor().execute("INSERT INTO demographics (userId, varAge1, varAge2, varAge3, varAge4, varAge5, varAge6, varAge7, varGender1, varGender2, varGender3, varGender4, varGender5, varGender6, varGender7) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                db.cursor().execute("INSERT INTO demographics (userId, varAge1, varAge2, varAge3, varAge4, varAge5, varAge6, varAge7, varGender1, varGender2, varGender3, varGender4, varGender5, varGender6, varGender7) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
                                     (user_id, int(checkbox_values[0]), int(checkbox_values[1]), int(checkbox_values[2]), int(checkbox_values[3]), int(checkbox_values[4]), int(checkbox_values[5]), int(checkbox_values[6]),
                                     int(checkbox_values[7]), int(checkbox_values[8]), int(checkbox_values[9]), int(checkbox_values[10]), int(checkbox_values[11]), int(checkbox_values[12]), int(checkbox_values[13])))
             
             else:
-                self.db.cursor().execute("INSERT INTO questionnaire_logs (userId, questionId, var1, var2, var3, var4, var5, var6) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",
+                db.cursor().execute("INSERT INTO questionnaire_logs (userId, questionId, var1, var2, var3, var4, var5, var6) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",
                                     (user_id, question_id, int(checkbox_values[0]), int(checkbox_values[1]), int(checkbox_values[2]), int(checkbox_values[3]), int(checkbox_values[4]), int(checkbox_values[5])))
-            self.db.commit()
+            db.commit()
 
             return True
         except Exception as ex:
@@ -65,8 +73,10 @@ class DataMgr():
 
     def log_elapsed_time(self, user_id, event_id, time_elapsed, block_id, trial_id):
         try:
-            self.db.cursor().execute("INSERT INTO elapsedtime_logs (userId, eventId, timeElapsed, blockId, trialId) VALUES(%s,%s,%s,%s,%s)", (user_id, event_id, time_elapsed, block_id, trial_id))
-            self.db.commit()
+            db = self.__connect_to_database()
+
+            db.cursor().execute("INSERT INTO elapsedtime_logs (userId, eventId, timeElapsed, blockId, trialId) VALUES(%s,%s,%s,%s,%s)", (user_id, event_id, time_elapsed, block_id, trial_id))
+            db.commit()
 
             return True
         except Exception as ex:
@@ -75,8 +85,10 @@ class DataMgr():
 
     def log_user_stuff(self, user_id, data):
         try:
-            self.db.cursor().execute("INSERT INTO logs (userId, data) VALUES(%s,%s)", (user_id, data))
-            self.db.commit()
+            db = self.__connect_to_database()
+
+            db.cursor().execute("INSERT INTO logs (userId, data) VALUES(%s,%s)", (user_id, data))
+            db.commit()
 
             return True
         except Exception as ex:
@@ -85,10 +97,12 @@ class DataMgr():
     
     def log_user_payment(self, user_id, payment_id):
         try:
+            db = self.__connect_to_database()
+
             payment_id = encrypt(payment_id, self.public_key)   # Encrypt payment_id
 
-            self.db.cursor().execute("INSERT INTO users_payout (userId, paymentId) VALUES(%s,%s)", (user_id, payment_id))
-            self.db.commit()
+            db.cursor().execute("INSERT INTO users_payout (userId, paymentId) VALUES(%s,%s)", (user_id, payment_id))
+            db.commit()
 
             return True
         except Exception as ex:
